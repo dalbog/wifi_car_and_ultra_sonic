486b6759df34461e4ca095a92735305f03619191
Leeseungjun070730 <40907669+Leeseungjun070730@users.noreply.github.com>
2019-05-02 16:21:55 +0900

    Initial commit

---
+++ wifi_car_and_ultra_sonic.ino
@@ -0,0 +1,47 @@
+//초음파 센서의 핀번호를 설정한다.
+int echoPin = 4;
+int trigPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+  // trig를 출력모드로 설정, echo를 입력모드로 설정
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT_PULLUP);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  //앞으로 가는 코드
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+
+
+  // 초음파를 보낸다. 다 보내면 echo가 HIGH 상태로 대기하게 된다.
+  digitalWrite(trigPin, LOW);
+  digitalWrite(echoPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // echoPin 이 HIGH를 유지한 시간을 저장 한다.
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
+  float distance = ((float)(340 * duration) / 10000) / 2;
+  // 수정한 값을 출력
+  Serial.print(distance);
+  Serial.println("cm");
+
+  if (distance < (float) 20) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+  }
+  delay(100);
+}
